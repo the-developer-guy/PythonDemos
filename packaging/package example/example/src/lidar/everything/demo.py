3f3932604b687818a748e1cd8aeb2a05b774b328
Durucz Béla <30567460+the-developer-guy@users.noreply.github.com>
2024-03-19 20:46:13 +0100

    Added more detailed import examples

--- packaging/package example/example/src/lidar/everything/demo.py
+++ packaging/package example/example/src/lidar/everything/demo.py
@@ -1,5 +1,11 @@
 def f():
-    print("f function")
+    print("f function, always available")
 
 def g():
-    print("g function")
+    print("g function, import * won't do")
+
+def h():
+    print("h function, have to do raw")
+
+def _i():
+    print("i function, should be hidden")
--- packaging/package example/import_demo.py
+++ packaging/package example/import_demo.py
@@ -2,6 +2,9 @@ from lidar.everything import *
 import lidar.everything
 import lidar.everything as et
 
+from lidar.everything.demo import *
+# or: from lidar.everything.demo import h
+
 # import * works for f(), because it was set in __all__
 f()
 
@@ -10,6 +13,11 @@ lidar.everything.g()
 
 # aliasing also works fine
 et.g()
+
+h()
+
+# _i is hidden from "import *"
+_i()
 
 # g was NOT included in __all__, this must fail
 g()
